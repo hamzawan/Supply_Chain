4ac01dd5b01c4bc74aed6822b4a6a4f62d0bc59f
hamzawan <hamzawan@github.com>
2019-10-01 11:31:15 +0500

    Update po

--- Supply_Chain_Proj/customer/views.py
+++ Supply_Chain_Proj/customer/views.py
@@ -1010,16 +1010,6 @@ def edit_delivery_challan_customer(request,pk):
             return HttpResponse(json.dumps({'message':"Item Already Exist"}))
         row = serializers.serialize('json',data)
         return HttpResponse(json.dumps({'row':row}))
-    # item_code_po_dc = request.POST.get('item_code_po_dc',False)
-    # if item_code_po_dc:
-    #     id = PoHeaderCustomer.objects.get(company, po_client = item_code_po_dc)
-    #     item_id = PoDetailCustomer.objects.filter(po_id = id).all()
-    #     for value in item_id:
-    #         row.append(value.item_id.id)
-    #     a = (str(row)[1:-1])
-    #     row = cursor.execute(f'''select * from inventory_add_products where id in ({a})''')
-    #     row = row.fetchall()
-    #     return JsonResponse({'row':row,'id':id.id})
     if request.method == 'POST':
         dc_detail.delete()
         edit_dc_customer =  request.POST.get('customer')
@@ -1043,7 +1033,7 @@ def edit_delivery_challan_customer(request,pk):
         items = json.loads(request.POST.get('items'))
         for value in items:
             item_id = Add_products.objects.get(id = value["id"])
-            dc_detail_update = DcDetailCustomer(item_id = item_id,description = value["description"] ,quantity = value["quantity"],accepted_quantity = 0, returned_quantity = 0, po_no = value["po_no"] ,dc_id = header_id)
+            dc_detail_update = DcDetailCustomer(item_id = item_id,description = value["description"] ,quantity = value["quantity"],accepted_quantity = 0, returned_quantity = 0, po_no = po_no ,dc_id = header_id)
             dc_detail_update.save()
         return JsonResponse({"result":"success"})
     return render(request,'customer/edit_delivery_challan_customer.html',{'dc_header':dc_header,'pk':pk,'dc_detail':dc_detail, 'all_item_code':all_item_code, 'all_accounts':all_accounts,'all_po_code':all_po_code,'allow_customer_roles':allow_customer_roles,'allow_supplier_roles':allow_supplier_roles,'allow_transaction_roles':allow_transaction_roles,'allow_inventory_roles':allow_inventory_roles,    'allow_report_roles':report_roles(request.user),'is_superuser':request.user.is_superuser})
